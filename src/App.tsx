9719632c706992d1f55cb48265287b21d157a61e
salino3 <programacionflavio@gmail.com>
2023-08-27 10:39:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <main>
+      <h1>Hola</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ src/pages/home/home.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './home.styles.scss';
+
+export const Home: React.FC = () => {
+
+  return (
+    <div>home.component</div>
+  )
+}
