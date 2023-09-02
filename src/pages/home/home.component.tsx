effca656ca5bc3ae207d829b898ed601138eb131
salino3 <programacionflavio@gmail.com>
2023-09-02 19:33:24 +0200

    add name and surname inputs and validations

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,233 +1,275 @@
-import React from 'react';
-import './home.styles.scss';
+import React from "react";
+import "./home.styles.scss";
 
 interface startValidationColorProps {
+  name: boolean | null;
+  surname: boolean | null;
   email: boolean | null;
   password: boolean | null;
   confirmPassword: boolean | null;
   age: boolean | null;
-  sex:  boolean | null;
-  dev:  boolean | null;
-};
+  sex: boolean | null;
+  dev: boolean | null;
+}
 
 export const Home: React.FC = () => {
-   const [startValidation, setStartValidation] = React.useState(false);
-   const [startValidationColor, setStartValidationColor] =
-     React.useState<startValidationColorProps>({
-       email: false,
-       password: false,
-       confirmPassword: false,
-       age: false,
-       sex: null,
-       dev: null,
-     });
+  const [startValidation, setStartValidation] = React.useState(false);
+  const [startValidationColor, setStartValidationColor] =
+    React.useState<startValidationColorProps>({
+      name: false,
+      surname: false,
+      email: false,
+      password: false,
+      confirmPassword: false,
+      age: false,
+      sex: null,
+      dev: null,
+    });
 
-   //!
-const [isEmailValid, setIsEmailValid] = React.useState< boolean | null>();
-const [firstValueAge, setFirstValueAge] = React.useState(false);
-const [isPasswordStrong, setIsPasswordStrong] = React.useState<boolean | null>();
-const [firstValuePswStrong, setFirstValuePswStrong] = React.useState<boolean | null>(false);
-const [isConfirmedPassword, setIsConfirmedPassword] = React.useState<boolean | null>();
-// const [firstValueConfirmedPsw, setFirstValueConfirmedPsw] = React.useState(false);
-const [firstValuedDev, setfirstValuedDev] = React.useState<boolean | null>(null)
- 
+  //!
+  const [isEmailValid, setIsEmailValid] = React.useState<boolean | null>();
+  const [firstValueName, setFirstValueName] = React.useState<boolean | null>(false);
+  const [firstValueSurname, setFirstValueSurname] = React.useState<boolean | null>(false);
+  const [firstValueAge, setFirstValueAge] = React.useState(false);
+  const [isPasswordStrong, setIsPasswordStrong] = React.useState<boolean | null>();
+  const [firstValuePswStrong, setFirstValuePswStrong] = React.useState<boolean | null>(false);
+  const [isConfirmedPassword, setIsConfirmedPassword] = React.useState<boolean | null>();
+  // const [firstValueConfirmedPsw, setFirstValueConfirmedPsw] = React.useState(false);
+  const [firstValuedDev, setfirstValuedDev] = React.useState<boolean | null>(
+    null
+  );
 
-   //!
+  //!
 
-   const [user, setUser] =
-     React.useState<any>({
-       email: "",
-       password: "",
-       confirmPassword: "",
-       age: null,
-       sex: "",
-       dev: "",
-     });
+  const [user, setUser] = React.useState<any>({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    age: null,
+    sex: "",
+    dev: "",
+  });
 
-   const [isMaleChecked, setIsMaleChecked] = React.useState(false);
-   const [isFemaleChecked, setIsFemaleChecked] = React.useState(false);
+  const [isMaleChecked, setIsMaleChecked] = React.useState(false);
+  const [isFemaleChecked, setIsFemaleChecked] = React.useState(false);
 
-   const onChangeSex = (sex: string) => {
-     if (sex === "Male") {
-       setUser({ ...user, sex: "Male" });
-       setIsMaleChecked(true);
-       setIsFemaleChecked(false);
-       setStartValidationColor({ ...startValidationColor, sex: true });
-     };
-     if (sex === "Female") {
-       setUser({ ...user, sex: "Female" });
-       setIsMaleChecked(false);
-       setIsFemaleChecked(true);
-       setStartValidationColor({ ...startValidationColor, sex: true });
-     };
-   };
+  const onChangeSex = (sex: string) => {
+    if (sex === "Male") {
+      setUser({ ...user, sex: "Male" });
+      setIsMaleChecked(true);
+      setIsFemaleChecked(false);
+      setStartValidationColor({ ...startValidationColor, sex: true });
+    }
+    if (sex === "Female") {
+      setUser({ ...user, sex: "Female" });
+      setIsMaleChecked(false);
+      setIsFemaleChecked(true);
+      setStartValidationColor({ ...startValidationColor, sex: true });
+    }
+  };
 
-   const register = (event: any) => {
-     event.preventDefault();
-     setStartValidation(true);
-     setStartValidationColor({
-       email: true,
-       password: true,
-       confirmPassword: true,
-       age: true,
-       sex: true,
-       dev: true,
-     });
+  const register = (event: any) => {
+    event.preventDefault();
+    setStartValidation(true);
+    setStartValidationColor({
+      name: true,
+      surname: true,
+      email: true,
+      password: true,
+      confirmPassword: true,
+      age: true,
+      sex: true,
+      dev: true,
+    });
 
-     if (user.age === null) {
-       setFirstValueAge(true);
-     };
+    if (user.age === null) {
+      setFirstValueAge(true);
+    }
 
-     if (user.password === "") {
-       setFirstValuePswStrong(true);
-     };
+    if (user.password === "") {
+      setFirstValuePswStrong(true);
+    }
 
-     if (user.dev === ""){
-      setfirstValuedDev(false)
-     };
+    if (user.dev === "") {
+      setfirstValuedDev(false);
+    }
 
-     if (!startValidationColor.sex){
-      setStartValidationColor({ ...startValidationColor , sex: false});
-     };
+    if (!startValidationColor.sex) {
+      setStartValidationColor({ ...startValidationColor, sex: false });
+    }
+
+    if (
+      isValidEmail() &&
+      isStrongPassword() &&
+      isPasswordConfirmed() &&
+      isMajor() &&
+      user.sex &&
+      user.dev
+    ) {
+      //  alert("Registering...");
+      console.log(JSON.stringify(user, null, 2));
+    } else {
+      //  alert("You have some error in the Form...");
+      console.log(user);
+    }
+
+    setStartValidation(false);
+  };
+
+  const isValidEmail = () => {
+    if (user.email !== "") {
+      setStartValidationColor({ ...startValidationColor, email: true });
+    }
+
+    const emailValid = user.email
+      ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email)
+      : null;
+
+    setIsEmailValid(emailValid);
+    console.log("isEmailValid", isEmailValid);
+    return emailValid;
+  };
+
+  const isStrongPassword = () => {
+    if (user.password !== "") {
+      setStartValidationColor({ ...startValidationColor, password: true });
+    }
+
+    const bool = user.password
+      ? /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9\d])(?=.*\d).{8,}/.test(
+          user.password
+        )
+      : null;
+    setIsPasswordStrong(bool);
+    return bool;
+  };
+
+  //
+  const isPasswordConfirmed = () => {
+    if (user.confirmPassword !== "" && user.password !== user.confirmPassword) {
+      setStartValidationColor({
+        ...startValidationColor,
+        confirmPassword: true,
+      });
+    }
+
+    const bool = user.confirmPassword
+      ? user.password === user.confirmPassword
+      : null;
+    setIsConfirmedPassword(bool);
+    return bool;
+  };
+
+  const isMajor = () => {
+    if (user.age && user.age < 18) {
+      const bool = user && user?.age && user?.age >= 18 ? true : null;
+      setStartValidationColor({ ...startValidationColor, age: bool });
+    }
+    if (user.age >= 18) {
+      setStartValidationColor({ ...startValidationColor, age: false });
+    }
+    return true;
+  };
+  // Handles
+
+  const handleName = (e: any) => {
+    setUser({ ...user, name: e.target.value });
+  };
+
+  React.useEffect(() => {
+    if (user.name) {
+      setFirstValueName(true);
+    };
+
+    if (user.name.length >= 2) {
+      setStartValidationColor({ ...startValidationColor, name: true });
+      setFirstValueName(false);
+    };
+    if (user.name.length < 2) {
+      setStartValidationColor({ ...startValidationColor, name: false });
+    };
+  }, [user.name, firstValueName, startValidationColor.name]);
 
 
-       if (
-         isValidEmail() &&
-         isStrongPassword() &&
-         isPasswordConfirmed() &&
-         isMajor() &&
-         user.sex &&
-         user.dev
-       ) {
-         //  alert("Registering...");
-         console.log(JSON.stringify(user, null, 2));
-       } else {
-         //  alert("You have some error in the Form...");
-         console.log(user);
-       }
+  const handleSurname = (e: any) => {
+    setUser({ ...user, surname: e.target.value });
+  };
 
-     setStartValidation(false);
-   };
+    React.useEffect(() => {
+      if (user.surname) {
+        setFirstValueSurname(true);
+      }
 
-   const isValidEmail = () => {
-     if (user.email !== "") {
-       setStartValidationColor({ ...startValidationColor, email: true });
-     }
+      if (user.surname.length >= 2) {
+        setStartValidationColor({ ...startValidationColor, surname: true });
+        setFirstValueSurname(false);
+      }
+      if (user.surname.length < 2) {
+        setStartValidationColor({ ...startValidationColor, surname: false });
+      }
+    }, [user.surname, firstValueSurname, startValidationColor.surname]);
 
-     const emailValid = user.email
-       ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email)
-       : null;
- 
-     setIsEmailValid(emailValid); 
-     console.log('isEmailValid', isEmailValid);
-     return emailValid;
-   };
 
-   const isStrongPassword = () => {
-     if (user.password !== "") {
-       setStartValidationColor({ ...startValidationColor, password: true });
-     }
-
-     const bool = user.password
-        ? /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9\d])(?=.*\d).{8,}/.test(
-            user.password
-          )
-        : null;
-        setIsPasswordStrong(bool);
-        return bool;
-   };
-
-   //
-   const isPasswordConfirmed = () => {
-     if (
-       user.confirmPassword !== "" &&
-       user.password !== user.confirmPassword
-     ) {
-       setStartValidationColor({
-         ...startValidationColor,
-         confirmPassword: true,
-       });
-     }
-
-      const bool = user.confirmPassword ? user.password === user.confirmPassword : null;
-      setIsConfirmedPassword(bool);
-       return bool;
-    //  return user.confirmPassword
-    //    ? user.password === user.confirmPassword
-    //    : null;
-   };
-
-   const isMajor = () => {
-      
-      if(user.age && user.age < 18){
-        const bool = user && user?.age && user?.age >= 18 ? true : null;
-        setStartValidationColor({ ...startValidationColor, age: bool });
-     }
-      if (user.age >= 18)  {
-       setStartValidationColor({ ...startValidationColor, age: false });
-     }
-     return true;
-   };
-   
-   const handleEmail = (e: any) => {
+  const handleEmail = (e: any) => {
     setUser({ ...user, email: e.target.value });
 
     isValidEmail();
-   }
+  };
 
-     React.useEffect(() => {
-       isValidEmail();
-     }, [isEmailValid, user.email]);
+  React.useEffect(() => {
+    isValidEmail();
+  }, [isEmailValid, user.email]);
 
-     //
-   const handleAge = (e: any) => {
-  setUser({ ...user, age: parseInt(e.target.value) });
+  //
+  const handleAge = (e: any) => {
+    setUser({ ...user, age: parseInt(e.target.value) });
 
-     isMajor();
-     setFirstValueAge(true);
-    };
-    
-    React.useEffect(() => {
-      isMajor();
-   }, [user.age]);
-   //
-    const handlePassword = (e: any) => {
-      setUser({ ...user, password: e.target.value });
+    isMajor();
+    setFirstValueAge(true);
+  };
 
-      isStrongPassword();
-    };
+  React.useEffect(() => {
+    isMajor();
+  }, [user.age]);
+  //
+  const handlePassword = (e: any) => {
+    setUser({ ...user, password: e.target.value });
 
-    React.useEffect(() => {
-      isStrongPassword();
-    }, [isPasswordStrong, user.password]);
+    isStrongPassword();
+  };
 
-//
-   const handleConfirmPassword = (e: any) => {
-     setUser({ ...user, confirmPassword: e.target.value });
+  React.useEffect(() => {
+    isStrongPassword();
+  }, [isPasswordStrong, user.password]);
 
-     isStrongPassword();
-   };
+  //
+  const handleConfirmPassword = (e: any) => {
+    setUser({ ...user, confirmPassword: e.target.value });
 
-    React.useEffect(() => {
-      isPasswordConfirmed();
-    }, [isConfirmedPassword, user.confirmPassword]);
-//
+    isStrongPassword();
+  };
 
-   const handleDev = (e: any) => {
-     setUser({ ...user, dev: e.target.value });
-console.log("handleDev", user.dev);
-setfirstValuedDev(true);
-    
-   };
-    React.useEffect(() => {
-       if (user.dev !== "") {
-         setStartValidationColor({ ...startValidationColor, dev: true });
-       } 
-       if (user.dev === "" && firstValuedDev) {
-         setStartValidationColor({ ...startValidationColor, dev: false });
-       }
-       console.log("handleDev2", startValidationColor.dev);
-    }, [isConfirmedPassword, user.dev]);
+  React.useEffect(() => {
+    isPasswordConfirmed();
+  }, [isConfirmedPassword, user.confirmPassword]);
+  //
+
+  const handleDev = (e: any) => {
+    setUser({ ...user, dev: e.target.value });
+    console.log("handleDev", user.dev);
+    setfirstValuedDev(true);
+  };
+  React.useEffect(() => {
+    if (user.dev !== "") {
+      setStartValidationColor({ ...startValidationColor, dev: true });
+    }
+    if (user.dev === "" && firstValuedDev) {
+      setStartValidationColor({ ...startValidationColor, dev: false });
+    }
+    console.log("handleDev2", startValidationColor.dev);
+  }, [isConfirmedPassword, user.dev]);
 
   return (
     <div>
@@ -253,7 +295,56 @@ setfirstValuedDev(true);
           Invalid email address!
         </span>
         <br />
-        {/* Problema1 */}
+        <input
+          className={`${
+            startValidationColor.name
+              ? "valid"
+              : startValidationColor.name === false && firstValueName
+              ? "invalid"
+              : ""
+          }`}
+          type="text"
+          value={user.name}
+          onChange={handleName}
+          id="name"
+          placeholder="Name.."
+        />
+        <br />
+        <span
+          className="errorText"
+          hidden={
+            !!startValidationColor.name ||
+            (!startValidationColor.name && !firstValueName)
+          }
+        >
+          You must refill name input
+        </span>
+        <br />
+        <input
+          className={`${
+            startValidationColor.surname
+              ? "valid"
+              : startValidationColor.surname === false && firstValueSurname
+              ? "invalid"
+              : ""
+          }`}
+          type="text"
+          value={user.surname}
+          onChange={handleSurname}
+          id="surname"
+          placeholder="Surname.."
+        />
+        <br />
+        <span
+          className="errorText"
+          hidden={
+            !!startValidationColor.surname ||
+            (!startValidationColor.surname && !firstValueSurname)
+          }
+        >
+          You must refill surname input
+        </span>
+        <br />
         <input
           className={
             isPasswordStrong === true
@@ -306,9 +397,9 @@ setfirstValuedDev(true);
               (user.age < 18 || startValidationColor.age === null)) ||
             isNaN(user.age) ||
             (user.age === null && firstValueAge)
-              ? "invalid"
+              ? "invalid invalidPseudoClass"
               : user && user?.age && user?.age >= 18
-              ? "valid"
+              ? "valid validPseudoClass"
               : ""
           }
           value={user.age || ""}
@@ -351,14 +442,14 @@ setfirstValuedDev(true);
           </select>
         </div>
         <br />
-        <div className='inputsCheckBox' >
+        <div className="inputsCheckBox">
           <label>Your sex:</label> &nbsp; &nbsp;
           <div
             className={`${
               startValidationColor.sex
                 ? "valid"
                 : startValidationColor.sex === false
-                ? "invalidPadding"
+                ? "invalid"
                 : ""
             } `}
           >
